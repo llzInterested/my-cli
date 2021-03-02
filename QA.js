8e13caefb651e010eec355a5eeecf633d360202d
lilz <lilz@tuya.com>
2021-03-02 17:29:04 +0800

    feat: 添加创建模板问答文件

---
+++ QA.js
@@ -0,0 +1,13 @@
+module.exports = [{
+  type: 'input',
+  name: 'author',
+  message: 'author:',
+}, {
+  type: 'input',
+  name: 'projectVersion',
+  message: 'version:',
+}, {
+  type: 'input',
+  name: 'projectDescription',
+  message: 'description:',
+}];
